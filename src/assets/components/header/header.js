4a79d6133a10b8c8fdb2a90233f6bea56943a55d
NorthUniverse <dennis.roshan@gmail.com>
2020-10-07 21:35:17 -0400

    woking logo image

--- src/assets/components/header/header.js
+++ src/assets/components/header/header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../../images/logo.PNG';
 
 function Header(props) {
     return (
         <React.Fragment>
             <div className="banner">
                 <div className="banner-image">
-                    <img src="../../images/logo.PNG" alt="banner picture"/>
+                    <img src= { logo } alt="banner picture"/>
                 </div>
                 <div className="banner-text">
                     Sleepora Mattresses
