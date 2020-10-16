1edb701dd0970664ec3e8c3751fb1c6ff9037ac9
NorthUniverse <dennis.roshan@gmail.com>
2020-10-15 23:10:51 -0400

    fixed footer link

--- src/assets/components/footer/Footer.js
+++ src/assets/components/footer/Footer.js
@@ -18,15 +18,6 @@ function Footer() {
                     <img src={ facebook } onClick={()=> window.open("https://www.facebook.com", "_blank")} alt="Facebook"/>
                     <img src={ linkedIn } onClick={()=> window.open("https://www.linkedin.com", "_blank")} alt="LinkedIn"/>
                     <img src={ instagram } onClick={()=> window.open("https://www.instagram.com", "_blank")} alt="Instagram"/>
-                    {/* <Link to="https://www.facebook.com">
-                        <img src={ facebook } alt="Facebook"/>
-                    </Link>
-                    <Link to="https://www.linkedin.com">
-                        <img src={ linkedIn } alt="LinkedIn"/>
-                    </Link>
-                    <Link to="https://www.instagram.com">
-                        <img src={ instagram } alt="Instagram"/>
-                    </Link> */}
                 </div>
             </div>
         </React.Fragment>
