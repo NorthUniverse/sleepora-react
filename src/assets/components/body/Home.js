5826a64db32e51ef41b0273b0cfa83625b8a7e83
NorthUniverse <dennis.roshan@gmail.com>
2020-10-19 22:27:03 -0400

    code so far

--- src/assets/components/body/Home.js
+++ src/assets/components/body/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import homeImage from '../../images/home.jpg';
+import '../../styles/minCss/home.css';
 
 function homepage() {
     return (
-        <div>
-            This is HomePage.
-        </div>
+        <React.Fragment>
+            <div className="body-home-background-image">
+                <img src={ homeImage } alt="mattress picture"/>
+            </div>
+            <div className="body-home-background-image-mobile">
+                <img src="images/gallery/gallery7.jpg" alt="mattress picture mobile"/>
+            </div>
+            <div className="body-home-text">
+                “Laugh and the world laughs with you, snore and you sleep alone.” ― Anthony Burgess
+            </div>
+        </React.Fragment>
     );
 }
 
