386110ce1230999888b50bba7b8712fdd8e880e8
NorthUniverse <dennis.roshan@gmail.com>
2020-10-19 22:48:44 -0400

    border added

--- src/assets/components/body/Home.js
+++ src/assets/components/body/Home.js
@@ -5,14 +5,16 @@ import '../../styles/minCss/home.css';
 function homepage() {
     return (
         <React.Fragment>
-            <div className="body-home-background-image">
-                <img src={ homeImage } alt="mattress picture"/>
-            </div>
-            <div className="body-home-background-image-mobile">
-                <img src="images/gallery/gallery7.jpg" alt="mattress picture mobile"/>
-            </div>
-            <div className="body-home-text">
-                “Laugh and the world laughs with you, snore and you sleep alone.” ― Anthony Burgess
+            <div className="content-wrapper">
+                <div className="body-home-background-image">
+                    <img src={ homeImage } alt="mattress picture"/>
+                </div>
+                <div className="body-home-background-image-mobile">
+                    <img src="images/gallery/gallery7.jpg" alt="mattress picture mobile"/>
+                </div>
+                <div className="body-home-text">
+                    “Laugh and the world laughs with you, snore and you sleep alone.” ― Anthony Burgess
+                </div>
             </div>
         </React.Fragment>
     );
