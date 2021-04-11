dd9d25d15af344094801c0096773c87e88bcb409
NorthUniverse <dennis.roshan@gmail.com>
2021-04-11 17:16:23 -0400

    contact.js

--- src/assets/components/body/Contact.js
+++ src/assets/components/body/Contact.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import homeImage from '../../images/home.jpg';
+import mobileHomeImage from '../../images/gallery/gallery7.jpg';
 
 function Contact() {
     return (
-        <div>
-            This is Contact us page.
-        </div>
+        <React.Fragment>
+            <div className="content-wrapper">
+                <div className="body-contact-wrapper">
+                    <div className="body-contact-background-image">
+                        <img src="images/contact.jpg" alt="bed picture"/>
+                    </div>
+                    <div className="body-contact-background-image-mobile">
+                        <img src="images/mobile-contact.jpg" alt="bed picture mobile"/>
+                    </div>
+                    <div className="body-contact-text">
+                        <div className="heading-wrapper">
+                            <p>The Mattress Shoppe</p>
+                        </div>
+                        <div className="p-wrapper">
+                            <p>#24, Ground floor, 5th main road, 18th Cross,<br/> BTM Layout 2nd Stage, N.S.Palya,<br/> Bangalore, 560 076, INDIA</p>
+                        </div>
+                        <div className="heading-wrapper">
+                            <p>Email</p>
+                        </div>
+                        <div className="p-wrapper">
+                            <p>mattressshoppe@rediffmail.com</p>
+                        </div>
+                        <div className="heading-wrapper">
+                            <p>Phone Number</p>
+                        </div>
+                        <div className="p-wrapper">
+                            <p>080-26784104 / 05</p>
+                        </div>
+                        <div className="heading-wrapper">
+                            <p>Store Hours</p>
+                        </div>
+                        <div className="p-wrapper">
+                            <p>Monday - Friday : 9am to 7pm<br/>Saturday : 10am to 5pm<br/>Sunday : Closed</p>
+                        </div>
+                        <div className="heading-wrapper">
+                            <p>Note: Store hours may vary due to Covid-19 pandemic</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
     );
 }
 
