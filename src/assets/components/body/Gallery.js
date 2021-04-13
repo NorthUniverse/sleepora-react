26091f55694331ca6d694446fdc3d4881730b290
NorthUniverse <dennis.roshan@gmail.com>
2021-04-12 21:05:19 -0400

    gallery working part 1

--- src/assets/components/body/Gallery.js
+++ src/assets/components/body/Gallery.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { GalleryItems } from './GalleryItems.js';
+import { NavLink } from 'react-router-dom';
+import '../../styles/minCss/galleryStyles.css';
 
 function Gallery() {
+    const imagePath = '../../images/gallery/';
     return (
-        <div>
-            This is Gallery Page.
-        </div>
+        <React.Fragment>
+            <div className="content-wrapper">
+                <div className="mobile-content-header">Gallery</div>
+                <div className="gallery-items-wrapper">
+
+                    <div className="gallery-each-item">
+                        {
+                        GalleryItems.map((item, index) => {
+                            const imgPath = require('../../images/gallery/' + item.imgName + '.jpg');
+                            return(
+                                <div>
+                                    <NavLink exact to="/" className="gallery-image-wrapper">
+                                        <img src = { imgPath } alt={item.alt + ` mattress picture`}/>
+                                    </NavLink>
+                                    <NavLink  exact to="/" className="gallery-image-text-wrapper">
+                                        Click here to enlarge
+                                    </NavLink>
+                                </div>
+                            )
+                        })
+                        }
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
     );
 }
 
