d9e20665b179700846ec8a40697da02838693fbb
NorthUniverse <dennis.roshan@gmail.com>
2021-04-12 22:21:21 -0400

    gallery v2

--- src/assets/components/body/Gallery.js
+++ src/assets/components/body/Gallery.js
@@ -8,27 +8,24 @@ function Gallery() {
     return (
         <React.Fragment>
             <div className="content-wrapper">
-                <div className="mobile-content-header">Gallery</div>
-                <div className="gallery-items-wrapper">
-
-                    <div className="gallery-each-item">
+                <div className="mobile-content-header">Gallery</div>                
+                    <div className="gallery-items-wrapper">
                         {
-                        GalleryItems.map((item, index) => {
-                            const imgPath = require('../../images/gallery/' + item.imgName + '.jpg');
-                            return(
-                                <div>
-                                    <NavLink exact to="/" className="gallery-image-wrapper">
-                                        <img src = { imgPath } alt={item.alt + ` mattress picture`}/>
-                                    </NavLink>
-                                    <NavLink  exact to="/" className="gallery-image-text-wrapper">
-                                        Click here to enlarge
-                                    </NavLink>
-                                </div>
-                            )
-                        })
+                            GalleryItems.map((item, index) => {
+                                const imgPath = require('../../images/gallery/' + item.imgName + '.jpg');
+                                return(
+                                    <div className="gallery-each-item">
+                                        <NavLink exact to={ imgPath } className="gallery-image-wrapper">
+                                            <img src = { imgPath } alt={item.alt + ` mattress picture`}/>
+                                        </NavLink>
+                                        <NavLink  exact to={ imgPath } className="gallery-image-text-wrapper">
+                                            Click here to enlarge
+                                        </NavLink>
+                                    </div>
+                                )
+                            })
                         }
                     </div>
-                </div>
             </div>
         </React.Fragment>
     );
---
+++ src/assets/components/body/GalleryItems.js
@@ -0,0 +1,51 @@
+const { default: Gallery } = require("../body/Gallery");
+
+export const GalleryItems = [{
+        imgName: 'gallery1',
+        alt: 'first'
+    },
+    {
+        imgName: 'gallery2',
+        alt: 'second'
+    },
+    {
+        imgName: 'gallery3',
+        alt: 'third'
+    },
+    {
+        imgName: 'gallery4',
+        alt: 'forth'
+    },
+    {
+        imgName: 'gallery5',
+        alt: 'fifth'
+    },
+    {
+        imgName: 'gallery6',
+        alt: 'sixth'
+    },
+    {
+        imgName: 'gallery7',
+        alt: 'seventh'
+    },
+    {
+        imgName: 'gallery8',
+        alt: 'eigth'
+    },
+    {
+        imgName: 'gallery9',
+        alt: 'ninth'
+    },
+    {
+        imgName: 'gallery10',
+        alt: 'tenth'
+    },
+    {
+        imgName: 'gallery11',
+        alt: 'eleventh'
+    },
+    {
+        imgName: 'gallery12',
+        alt: 'twelvth'
+    }
+]
