1257998e769daaeae025a431e0f599ec08cf27e4
NorthUniverse <dennis.roshan@gmail.com>
2021-04-17 13:34:07 -0400

    code so far

--- src/assets/components/body/Gallery.js
+++ src/assets/components/body/Gallery.js
@@ -14,19 +14,18 @@ function Gallery() {
                 <div className="mobile-content-header">Gallery</div>                
                     <div className="gallery-items-wrapper">
                         <SRLWrapper>
-                        {
-                            
-                            GalleryItems.map((item, index) => {
-                                const imgPath = require('../../images/gallery/' + item.imgName + '.jpg');
-                                return(
-                                    <div className="gallery-each-item">
-                                        <div className="gallery-image-wrapper">
-                                            <img src = { imgPath } className="each-image-item" alt={`Gallery Image ` + item.alt}/>
+                            {
+                                GalleryItems.map((item, index) => {
+                                    const imgPath = require('../../images/gallery/' + item.imgName + '.jpg');
+                                    return(
+                                        <div className="gallery-each-item">
+                                            <div className="gallery-image-wrapper">
+                                                <img src = { imgPath } className="each-image-item" alt={`Gallery Image ` + item.alt}/>
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            })
-                        }
+                                    )
+                                })
+                            }
                         </SRLWrapper>
                     </div>
             </div>
