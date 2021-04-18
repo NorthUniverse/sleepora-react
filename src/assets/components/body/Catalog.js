bc9cb177d70ab77c7b75dc32224ddce15a01eda9
NorthUniverse <dennis.roshan@gmail.com>
2021-04-18 14:25:48 -0400

    catalog so far

--- src/assets/components/body/Catalog.js
+++ src/assets/components/body/Catalog.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { CatalogItems } from './CatalogItems.js';
+import '../../styles/minCss/catalogStyles.css';
 
 function Catalog() {
     return (
-        <div>
-            This is Catalog Page.
-        </div>
+        <React.Fragment>
+            <div className="content-wrapper">
+                <div className="mobile-content-header">Gallery</div>          
+                    <div className="catalog-items-wrapper">
+                        {
+                            CatalogItems.map((item, index) => {
+                                const imgPath = require('../../images/catalog/' + item.name + '.jpg');
+                                let mattressName = item.name;
+                                if(mattressName.includes("-")) {
+                                    let mattressArray = mattressName.split("-");
+                                    mattressName = mattressArray[0] + " " + mattressArray[1];
+                                }
+                                return(
+                                    <div className="catalog-each-item">
+                                        <div className="catalog-image-wrapper">
+                                            <img src = { imgPath } alt={ mattressName + ` Mattress`}/>
+                                        </div>
+                                        <div className="catalog-image-text-wrapper">{mattressName + ` Mattress`}</div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+            </div>
+        </React.Fragment>
     );
 }
 
---
+++ src/assets/components/body/CatalogItems.js
@@ -0,0 +1,79 @@
+const { default: Catalog } = require("../body/Catalog");
+
+export const CatalogItems = [{
+        name: 'Paradise',
+        typeName: 'Bonnell Spring Mattress',
+        description: [
+            "Soft Knitted Fabric quilted with pure foam for better comfort",
+            "Bonnell Inner spring provides firm back support",
+            "Edge guard springs provides good edge support and durability",
+            "5 Years warranty"
+        ]
+    },
+    {
+        name: 'Stream',
+        typeName: 'Pillow Top Bonnell Spring Mattress',
+        description: [
+            "Soft Knitted Fabric quilted with pure foam for better comfort",
+            "Pillow top designed for superior comfort",
+            "Bonnell Inner spring provides firm back support",
+            "Edge guard springs provides good edge support and durability",
+            "5 Years warranty"
+        ]
+    },
+    {
+        name: 'Sunrise',
+        typeName: 'Euro Top Bonnell Spring Mattress',
+        description: [
+            "Soft Knitted Fabric quilted with pure foam for better comfort",
+            "Eurotop 2\" for extra superior cosiness",
+            "Bonnell Inner spring provides firm back support",
+            "Edge guard springs provides good edge support and durability",
+            "5 Years warranty"
+        ]
+    },
+    {
+        name: 'Revital',
+        typeName: 'Ortho Mattress',
+        description: [
+            "Combination of Rebonded Foam, Coir and PU foam mattress",
+            "Soft Knitted Fabric quilted with pure foam for luxurious comfort",
+            "Rebonded foam & coir for good orthopedic support & comfort to spine",
+            "Reversible, hard surface on one side and regular on the other",
+            "5 Years warranty"
+        ]
+    },
+    {
+        name: 'Miracle',
+        typeName: 'Pocket Spring Mattress',
+        description: [
+            "Soft Knitted Fabric quilted with pure foam for luxurious comfort",
+            "Individual Pocket Spring provides perfect body support with better weight distribution & disturbance free motion transfer for complete restful sleep",
+            "Edge guard springs provides good edge support and durability",
+            "Reversible with regular surface on both sides",
+            "7 Years warranty"
+        ]
+    },
+    {
+        name: 'Wave',
+        typeName: 'Pillow Top Pocket Spring Mattress',
+        description: [
+            "Soft Knitted Fabric quilted with pure foam for luxurious comfort",
+            "Pillow top designed for superior comfort",
+            "Individual Pocket Spring provides perfect body support with better weight distribution & disturbance free motion transfer for complete restful sleep",
+            "Edge guard springs provides good edge support and durability",
+            "7 Years warranty"
+        ]
+    },
+    {
+        name: 'Snow-Top',
+        typeName: 'Euro Top Pocket Spring Mattress',
+        description: [
+            "Soft Knitted Fabric quilted with pure foam for luxurious comfort",
+            "Euro top 2\" for extra superior comfort and ultimate cosiness",
+            "Individual Pocket Spring provides perfect body support with better weight distribution & disturbance free motion transfer for complete restful sleep",
+            "Edge guard springs provides good edge support and durability",
+            "7 Years warranty"
+        ]
+    }
+]
